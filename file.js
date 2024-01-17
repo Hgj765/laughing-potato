f313907f916e88dae196ebbb4cc454e1de671d08
NtiElev-27 <156200054+Hgj765@users.noreply.github.com>
2024-01-17 14:45:09 +0100

    Create file.js

---
+++ file.js
@@ -0,0 +1,49 @@
+i=0;
+function Add() {document.getElementById('numr').innerHTML= parseInt(document.getElementById("nmre").value)+parseInt(document.getElementById("nmrt").value);}
+function Mul() {document.getElementById('numr').innerHTML= parseInt(document.getElementById("nmre").value)*parseInt(document.getElementById("nmrt").value);}
+function Pow() {document.getElementById('numr').innerHTML= parseInt(document.getElementById("nmre").value)^parseInt(document.getElementById("nmrt").value);}
+
+
+
+function Kenobi() {
+    if (i==1){document.getElementById('demo').innerHTML='General kenobi';}
+    if (i==2){document.getElementById('demo').innerHTML='General Kenobi. You are a bold one.  Kill him!';}
+    if (i==3){document.getElementById('demo').innerHTML='Back away! I will deal with this Jedi slime myself.';}
+    if (i==4){document.getElementById('demo').innerHTML='Your move';}
+    if (i==5){document.getElementById('demo').innerHTML='You fool. I\'ve been trained in your Jedi arts by Count Dooku.';}
+    if (i==6){document.getElementById('demo').innerHTML='Attack, Kenobi!';}
+
+
+i+=1;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (i==2){document.getElementById('demo').innerHTML='General Kenobi. You are a bold one.  Kill him!';}
+if (i==3){document.getElementById('demo').innerHTML='Back away! I will deal with this Jedi slime myself.';}
+if (i==4){document.getElementById('demo').innerHTML='Your move';}
+if (i==5){document.getElementById('demo').innerHTML='You fool. I\'ve been trained in your Jedi arts by Count Dooku.';}
+if (i==6){document.getElementById('demo').innerHTML='Attack, Kenobi!';}
